5d41012b0f0a73fc96a9478e1d141d1df06c3eaa
chen xinfeng <flychen50@gmail.com>
2014-03-20 16:41:55 +0800

    add reverse sentense with sequence words

--- reverseSentense.cpp
+++ reverseSentense.cpp
@@ -1,29 +1,48 @@
 #include <stdio.h>
-char* reverseSentense(const char* src){
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+char* reverseSentense( char* src){
+  // char* src = con_src;
   const int wordlen = 15;
   const int wordcount =100;
-  char** twoArray = new char[wordcount][wordlen];
+  //char** twoArray = new char*[wordlen];
+  char twoArray[wordcount][wordlen]={'\0'};
   int wordpos,letterpos,sentenselen;
-  for(wordpos=0,letterpos=0;*src="\n";src++){
-    if(*scr==""){
+  cout<<src<<endl;
+  for(wordpos=0,letterpos=0;*src!='\0';src++){
+    if(*src==' '){
+     
+      // printf("%s",twoArray[wordpos]);
       wordpos++;
       letterpos=0;
+      // *src='*';
     }
-    twoArray[wordpos][letterpos]=*src;
+    //  if(twoArray[wordpos]==NULL){
+    //twoArray[wordpos]=new char[wordlen];
+    //}
+    twoArray[wordpos][letterpos++]=*src;
+    // cout<<twoArray[wordpos]<<endl;
     sentenselen++;
 }
-  char* reverse=(char*)malloc(setenselen);
+  char* reverse=(char*)calloc(sentenselen+1,sizeof(char));
   char* currpos = reverse;
+ 
   for(;wordpos>=0;wordpos--){
-    letterpos = 0;
-    while(twoArray[wordpos][letterpos]!="\0"){
-      *currpos++=twoArray[wordpos][letterpos];
-}}
+   cout<<wordpos<<endl;
+   letterpos = 0;
+    while(twoArray[wordpos][letterpos]!='\0'){
+      *currpos++ = twoArray[wordpos][letterpos];
+      // cout<<twoArray[wordpos]<<endl;
+      letterpos++;
+
+ }
+}
   return reverse;
 }
 int main(int argc,char** argv){
   char* sentense = "I am a stundent";
-  std::cout<<reverseSentense(sentense)<<std::endl;
+  cout<<"reverse sentense:"<<reverseSentense(sentense)<<std::endl;
 
 
 }
