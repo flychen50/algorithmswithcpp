9211a29826299bdc67d84a75b46c8e5379fcec6a
cxf <cxf@lyhdeMacBook-Air.local>
2017-06-25 17:18:05 +0800

    update link merge

--- sort/link_mergesort.c
+++ sort/link_mergesort.c
@@ -7,6 +7,29 @@ struct node {Item item;link next;};
 #define exch(A,B) {Item t = A;A=B;B=t;}
 #define min(A,B) ((A)<(B)?(A):(B))
 
+link merge(link a,link b){
+  struct node head;link c = &head;
+  while(a!=NULL  && b!=NULL){
+    if(less(a->item,b->item)){
+      c->next = a;c = a;a = a->next;
+    }else{
+      c->next = b;c = b;b = b->next;
+    }
+  }
+  c->next = (a==NULL)?b:a;
+  return head.next;
+}
+
+link linkmergesort(link c){
+  link a,b;
+  if(c==NULL || c->next == NULL) return c;
+  a = c;b = c->next;
+  while(b != NULL && b->next!=NULL) {
+    c = c->next; b  = b->next->next;
+  }
+  b = c->next;c->next = NULL;
+  return merge(linkmergesort(a),linkmergesort(b));
+}
 
 
 int main(int argc,char** argv){
@@ -31,18 +54,22 @@ int main(int argc,char** argv){
   printf("\n origin data\n");
 
 
-  b = &headb;b->next = NULL;
-  for(t = a->next;t!=NULL;t = u){
-    u = t->next;
-    for(x = b;x->next !=NULL;x = x->next){
-      if(x->next->item > t->item) break;
-    }
-    t->next = x->next;x->next = t;
-  }
-
-  for(t = b->next;t!=NULL;t = t->next){
+  /* b = &headb;b->next = NULL; */
+  /* for(t = a->next;t!=NULL;t = u){ */
+  /*   u = t->next; */
+  /*   for(x = b;x->next !=NULL;x = x->next){ */
+  /*     if(x->next->item > t->item) break; */
+  /*   } */
+  /*   t->next = x->next;x->next = t; */
+  /* } */
+  link c = a->next;
+link d =linkmergesort(c);
+  for(t = d;t!=NULL;t = t->next){
     printf("%d ",t->item);
   }
+  /* for(t = b->next;t!=NULL;t = t->next){ */
+  /*   printf("%d ",t->item); */
+  /* } */
   printf("\nmergesort\n");
   return 0;
 }
