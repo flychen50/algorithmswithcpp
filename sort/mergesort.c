d92641e60a0c27c4a62b537c996fec04a9571b6c
cxf <cxf@lyhdeMacBook-Air.local>
2017-06-25 08:59:14 +0800

    add mege

--- sort/mergesort.c
+++ sort/mergesort.c
@@ -3,7 +3,7 @@
 typedef int Item;
 #define less(A,B) ((A)<(B))
 #define exch(A,B) {Item t = A;A=B;B=t;}
-
+#define min(A,B) ((A)<(B)?(A):(B))
 void merge(Item a[],int l,int m ,int r){
   Item aux[r-l];
   int i,j,k;
@@ -25,6 +25,16 @@ void mymergesort(Item a[],int l,int r){
   merge(a,l,m,r);
 }
 
+void mergeBU(Item a[],int l ,int r){
+  int i,m;
+  for(m=1;m<r-l;m = m+m){
+    for(i= l;i<=r-m;i +=m+m){
+      merge(a,i,i+m-1,min(i+m+m-1,r));
+    }
+  }
+}
+
+
 
 int main(int argc,char** argv){
 
@@ -38,7 +48,8 @@ int main(int argc,char** argv){
     a[i] = 10000*(1.0*rand()/RAND_MAX);
   }
 
-  mymergesort(a,0,list_range-1);
+  //  mymergesort(a,0,list_range-1);
+  mergeBU(a,0,list_range-1);
   for(int i=0;i<100;i++){
     printf("%d ",a[i]);
   }
